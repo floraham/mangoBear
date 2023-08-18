5a175c5b278ea890859b9bcb6f3695af580ef527
floraham <floraham@outlook.com>
2023-08-18 14:27:39 -0700

    Initial commit

---
+++ R/burritos_cost.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param price Price per burrito in dollars
+#' @param count Number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+burritos_cost <- function(price, count) {
+  price * count
+}
---
+++ R/zoobears_cost.R
@@ -0,0 +1,15 @@
+#' Title ZooBears Cost
+#'
+#' @param price price of a zoobear
+#' @param count number of bears at the zoo
+#'
+#' @return Returns the cost of total zoobears entered
+#' @export
+#'
+#' @examples
+zoobears_cost <- function(price, count) {
+  final = price * count
+  print(paste0("your zoobear costs", final, "dollars"))
+}
+
+
